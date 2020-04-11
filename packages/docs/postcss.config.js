bf728d4f73ae95e619b7ff84af6a5ce36e448eb6
tol.is <me@tol.is>
2020-04-11 11:22:33 +0100

    docs pkg working

--- packages/docs/postcss.config.js
+++ packages/docs/postcss.config.js
@@ -1,22 +1,23 @@
 const tailwindcss = require('tailwindcss');
 const { themeCompositor } = require('@styled-rhythm/tailwindcss');
+const themePlex = require('@styled-rhythm/theme-plex');
 
-const themePlex = require('./theme');
-themePlex.fonts = {
-	mono: [
-		{
-			familyName: 'JetBrainsMono',
-			fallback: 'monospace',
-			upm: 1000,
-			xHeight: 486,
-			capHeight: 710,
-			ascent: 978,
-			descent: -258,
-			weight: 400,
-			italic: false,
-		},
-	],
-};
+// const themePlex = require('./theme');
+// themePlex.fonts = {
+// 	mono: [
+// 		{
+// 			familyName: 'JetBrainsMono',
+// 			fallback: 'monospace',
+// 			upm: 1000,
+// 			xHeight: 486,
+// 			capHeight: 710,
+// 			ascent: 978,
+// 			descent: -258,
+// 			weight: 400,
+// 			italic: false,
+// 		},
+// 	],
+// };
 
 const tailwindConfig = {
 	theme: {},
--- packages/docs/src/pages/index.js
+++ packages/docs/src/pages/index.js
@@ -4,7 +4,7 @@ const IndexPage = () => {
 	return (
 		<article className="bg-baseline pt-10">
 			<div>
-				<p className="mono-400 type-12/4">Styled Rhythm</p>
+				<p className="font-sans-400 type-12/4">Styled Rhythm</p>
 			</div>
 			<div className="h-14"></div>
 		</article>
