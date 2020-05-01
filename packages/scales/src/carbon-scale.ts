43838d56c37b2947c5d25dd66d425c56595005ac
tol.is <me@tol.is>
2020-05-01 11:36:21 +0100

    scales types

--- packages/scales/src/carbon-scale.ts
+++ packages/scales/src/carbon-scale.ts
@@ -1,15 +1,24 @@
-export const ibmCarbonScale = (params: any = {}) => {
+type modularScaleParams = {
+	base: number;
+	interval: number;
+	increment: number;
+};
+
+export const ibmCarbonScale = (params: modularScaleParams): Function => {
 	const { base = 16, interval = 4, increment = 2 } = params;
 
-	const computeFontSize = step => {
+	const computeFontSize = (step: number): number => {
 		if (step <= 1) {
 			return base;
 		}
 
-		return computeFontSize(step - 1) + Math.floor((step - 2) / interval + 1) * increment;
+		return (
+			computeFontSize(step - 1) +
+			Math.floor((step - 2) / interval + 1) * increment
+		);
 	};
 
-	return step => computeFontSize(step + 1);
+	return (step: number): number => computeFontSize(step + 1);
 };
 
 export default ibmCarbonScale;
--- packages/scales/src/modular-scale.ts
+++ packages/scales/src/modular-scale.ts
@@ -1,9 +1,15 @@
 import { RATIOS } from './ratios';
 
-export const modularScale = (params: any = {}) => {
+type modularScaleParams = {
+	base: number;
+	ratio: number;
+	interval: number;
+};
+
+export const modularScale = (params: modularScaleParams) => {
 	const { base = 16, ratio = RATIOS.PERFECT_FOURTH, interval = 2 } = params;
 
-	return step => {
+	return (step: number) => {
 		return Math.floor(base * Math.pow(ratio, step / interval));
 	};
 };
